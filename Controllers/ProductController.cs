792e81a73683958164dd1a60d3c26ed8fdb8cea1
Narayan jena <98013623+NJena404@users.noreply.github.com>
2023-06-23 14:18:56 +0530

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using OfficeOpenXml;
+
+public class ProductController : Controller
+{
+    private const string ConnectionString = "mongodb://localhost:27017";
+    private const string DatabaseName = "ExcelData";
+    private const string CollectionName = "Exceldatas";
+
+    public ActionResult Upload()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Upload(HttpPostedFileBase file)
+    {
+        if (file != null && file.ContentLength > 0)
+        {
+            using (var package = new ExcelPackage(file.InputStream))
+            {
+                var worksheet = package.Workbook.Worksheets[1]; 
+                var rowCount = worksheet.Dimension.Rows;
+                var dataList = new List<ExcelData>();
+
+                for (var row = 2; row <= rowCount; row++) // Start from row 2 to skip header
+                {
+                    dataList.Add(new ExcelData
+                    {
+                        Id = worksheet.Cells[row, 1].Value?.ToString(),
+	        device_id = worksheet.Cells[row, 2].Value?.ToString(),
+                        power_type = worksheet.Cells[row, 3].Value?.ToString(),
+                        description= worksheet.Cells[row, 4].Value?.ToString(),
+                        event_date= worksheet.Cells[row, 5].Value?.ToString(),
+	        added_on= worksheet.Cells[row, 6].Value?.ToString()
+                    });
+                }
+
+               
+                var client = new MongoClient(ConnectionString);
+                var database = client.GetDatabase(DatabaseName);
+                var collection = database.GetCollection<ExcelData>(CollectionName);
+                collection.InsertMany(dataList);
+            }
+        }
+
+        return RedirectToAction("Upload");
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MongoDbCRUD.Models
+{
+    public class ExcelData
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [Required]
+        [BsonElement("device_id")]
+        public string device_id{ get; set; }
+
+        [Required]
+        [BsonElement("power_type")]
+        public int power_type{ get; set; }
+
+        [Required]
+        [BsonElement("description")]
+        public string description{ get; set; }
+
+        [BsonElement("event_date")]
+        public DateTime event_date{ get; set; }
+
+        [BsonElement("added_on")]
+        public DateTime added_on{ get; set; }
+    }
+}
